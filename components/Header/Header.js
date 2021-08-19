9a5ffbae9366e40f44f5c53ecf2c4c503cbb3ce8
sadat <sadat0akhtar@gmail.com>
2021-08-19 23:13:02 +0100

    added user icon and styled headerTop component

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -2,7 +2,7 @@ import HeaderTop from "./HeaderTop"
 
 function Header() {
     return (
-        <div className=''>
+        <div className='bg-[#0053a0]'>
             {/* headerTop */}
             <HeaderTop />
             {/* headerBbottom */}
--- components/Header/HeaderTop.js
+++ components/Header/HeaderTop.js
@@ -2,12 +2,13 @@ import Image from 'next/image'
 // import { QuestionMarkCircleIcon } from '@heroicons/react/outline'
 // import QuestionMarkCircle from '../../assets/questionMarkCircle.png'
 import Switch from '@material-ui/core/Switch'; 
+import PersonIcon from '@material-ui/icons/Person';
 
 function HeaderTop() {
     return (
         <div className='flex justify-between  bg-[#0053a0]'>
-            <div className='flex text-white p-2'>
-                <h3 className='font-bold text-sm'>Need Help</h3>
+            <div className='flex text-white p-2 hidden sm:flex'>
+                <h3 className='font-bold text-sm '>Need Help</h3>
                 <img
                     className='object-contain rounded-full ml-1' 
                     src='https://res.cloudinary.com/dstsblg8x/image/upload/v1629407495/questionMarkCircle_fkuvc0.png' 
@@ -16,7 +17,7 @@ function HeaderTop() {
                     alt='question mark'
             />
             </div>
-            <div className='flex justify-center items-center text-white font-bold text-sm mr-2'>
+            <div className='flex justify-center items-center text-white font-bold text-sm mr-2 hidden sm:flex'>
                 <h3>INC VAT</h3>
                 <Switch
                     defaultChecked
@@ -25,6 +26,10 @@ function HeaderTop() {
       />
                 
                 <h3>EX VAT</h3>
+            </div>
+            <div className='flex justify-center items-center pl-2 sm:hidden'>
+                <PersonIcon color="action" fontSize="small"/>
+                <h3 className='text-white text-xs p-2 '>sign in / Regi...</h3>
             </div>
            
             
