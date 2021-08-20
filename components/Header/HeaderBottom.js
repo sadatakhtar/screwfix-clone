4f13d5c30a57a11af2714427d4d4a679cc53f762
sadat <sadat0akhtar@gmail.com>
2021-08-20 12:58:55 +0100

    styled HeaderBottom component

--- components/Header/HeaderBottom.js
+++ components/Header/HeaderBottom.js
@@ -12,33 +12,33 @@ function HeaderBottom() {
         <Image src={screwfixLogo} width={220} height={80} objectFit="cover"/>
       </div>
 
-      <div className='flex justify-evenly space-x-5  m-2 p-2'>
+      <div className='flex justify-evenly space-x-5  m-2 p-2 hidden sm:flex'>
           <div className='flex justify-center items-center space-x-2 border-l-2'>
-              <PersonIcon color={"primary"} fontSize={"large"}/>
+              <PersonIcon style={{ fill: "#0053a0"}} fontSize={"large"}/>
               <div>
-                  <h3 className='text-blue-900 font-bold'>Your Account</h3>
-                  <h4 className='text-xs'>Sign in / Reg...</h4>
+                  <h3 className='text-[#0053a0] font-bold'>Your Account</h3>
+                  <h4 className='text-xs text-[#636363]'>Sign in / Reg...</h4>
               </div>
           </div>
           <div className='flex justify-center items-center space-x-2 border-l-2'>
-          <LocationOnIcon color={"primary"} fontSize={"large"}/>
+          <LocationOnIcon style={{ fill: "#0053a0"}} fontSize={"large"}/>
               <div>
-                  <h3 className='text-blue-900 font-bold'>Store Locator</h3>
-                  <h4 className='text-xs'>Select a store</h4>
+                  <h3 className='text-[#0053a0] font-bold'>Store Locator</h3>
+                  <h4 className='text-xs text-[#636363]'>Select a store</h4>
               </div>
           </div>
           <div className='flex flex-col justify-center items-center space-x-2 border-l-2'>
-          <ShoppingBasketIcon color={"primary"} fontSize={"large"}/>
+          <ShoppingBasketIcon style={{ fill: "#0053a0"}} fontSize={"large"}/>
               <div>
-                  <h3 className='text-blue-900 font-bold'>Checkout</h3>
+                  <h3 className='text-[#0053a0] font-bold'>Checkout</h3>
         
               </div>
           </div>
       </div>
 
       <div className="flex bg-white text-blue-900 text-sm font-bold sm:hidden">
-        <div className="pb-7">
-          <h3>INC</h3>
+        <div className="text-xs pb-7 text-[#0053a0]">
+          <h3 >INC</h3>
           <h3>VAT</h3>
         </div>
 
@@ -47,7 +47,7 @@ function HeaderBottom() {
           color="default"
           inputProps={{ "aria-label": "checkbox with default color" }}
         />
-        <div>
+        <div className='text-[#0053a0] text-xs'>
           <h3>EX</h3>
           <h3>VAT</h3>
         </div>
