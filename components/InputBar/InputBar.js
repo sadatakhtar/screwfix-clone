c1529c46a8bc2f7f8c16e976db54b207cdb64a28
sadat <sadat0akhtar@gmail.com>
2021-08-21 13:54:07 +0100

    tweaked

--- components/InputBar/InputBar.js
+++ components/InputBar/InputBar.js
@@ -3,7 +3,7 @@ import SearchIcon from '@material-ui/icons/Search';
 function InputBar() {
     return (
         <div className='flex border p-2 ' >
-          <input className='p-4 border-r-2  m-auto outline-none h-10 w-full' type='text' placeholder='What are you looking for?'/>
+          <input className='p-4 border-r-2  m-auto outline-none h-10 w-full ' type='text' placeholder='What are you looking for?'/>
           <SearchIcon className='mt-2 ml-2' fontSize='medium'/>
 
       </div>
