2d13cb53545ef24c10e19d444bfb1fddb46a37c4
sadat <sadat0akhtar@gmail.com>
2021-08-22 13:06:53 +0100

    tweaked styling

--- components/BottomNav/BottomNav.js
+++ components/BottomNav/BottomNav.js
@@ -1,12 +1,12 @@
 function BottomNav() {
   return (
-    <div className='flex justify-evenly text-[#0053a0] text-sm font-bold p-1 '>
+    <div className='flex justify-evenly text-[#0053a0] text-sm font-bold p-1 tracking-tighter'>
       <div className=" animate-pulse sm:animate-none">
         <h4>CLICK & COLLECT</h4>
       </div>
 
       <div className='hidden sm:flex' >
-          <h4>FREE DELIVERY</h4>
+          <h4>FREE DELIVERY OVER £50</h4>
       </div>
 
       <div className='hidden sm:flex'>
